65352ac5145b9fc9a4a81bd7aedd6da0290b0789
Michael Dik <michaeldik@Michaels-MacBook-Pro.local>
2020-11-09 18:20:34 -0500

    Java

---
+++ Java/variables_txt.java
@@ -0,0 +1,30 @@
+// String  | int | float | char | boolean
+
+String name ="John";
+System.out.println(name);
+int myNum = 5;
+System.out.println(myNum);
+
+int otherNum;
+int otherNum = 15;
+
+int thirdNum = 15;
+thirdNum = 20 ;  // ThirdNum is now 20
+
+
+
+final finalNum = 15;
+myNum = 20; // This will generate an error: cannot assign a value to a final variable
+
+
+// Andddd Wrapup
+
+int num = 0;
+float myFloatNum = 5.99f;
+char myLetter = 'D';
+boolean myBool= true;
+String myText = "Hello";
+
+
+// Primitive Data Types
+byte | short | int | long | double | boolean | char
